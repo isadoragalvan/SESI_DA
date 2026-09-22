79553d6d03484543a462b12a204868459e0c1dad
isadora <isadora.galvan@aluno.senai.br>
2026-09-22 10:04:57 -0300

    cadastro

--- AULA_08/script.js
+++ AULA_08/script.js
@@ -1,24 +1,75 @@
-//const nome = localStorage.getItem("nome");
+function login() {
+    // 1º Acessar o valor digitado nos campos USUARIO e SENHA
+    const campo_usuario = document.getElementById("usuario").value;
+    const campo_senha = document.getElementById("senha").value;
 
-//alert(nome);
+    // 2º carregar os valores do localStorange
+    const local_usuario = localStorage.getItem("usuario");
+    const local_senha = localStorage.getItem("senha");
 
-//localStorage.setItem("nome", "Jurema");
-
-//alert(localStorage.getItem("nome"));
-
-//localStorage.removeItem("nome");
-
-function login(){
-
-    const campo_usuario = localStorage.getElementById("usuario").value;
-    const campo_senha = localStorage.getElementById("senha").value;
-
-    const local_usuario = document.getId("usuario");
-    const local_senha = document.getId("senha");
-
-    if(campo_usuario == local_usuario){
-        alert("Login realizado com sucesso!");
-    }else{
-        alert("Usuário Inválido");
+    // 3º Validar se o valores digitados são iguais aos valores armazenados no localStorage
+    if (campo_usuario == local_usuario && campo_senha == local_senha) {
+        alert("Login realizado com sucesso! 👍");
+    } else {
+        alert("Usuário ou senha inválidos! 👎");
     }
+
+}
+
+
+function cadastro() {
+        // 1º Carregar os campos de cadastro
+        // NOME, USUÁRIO, SENHA, PALAVRA-PASSE
+        const campo_nome = document.getElementById("nome").value;
+        const campo_usuario = document.getElementById("usuario").value;
+        const campo_senha = document.getElementById("senha").value;
+        const campo_palavrapasse = document.getElementById("palavrapasse").value;
+
+
+        // 2º Cadastrar os dados no localStorage
+        // Ex.: localStorage.setItem("NOME", valor)
+        // OBS.: "valor" é o dado que foi carregado no passo 1
+        localStorage.setItem("nome", campo_nome)
+        localStorage.setItem("usuario", campo_usuario)
+        localStorage.setItem("senha", campo_senha)
+        localStorage.setItem("palavrapasse", campo_palavrapasse)
+
+        // 3º Redirecionar para a tela de login
+        window.location.href = "login.html"
+}
+
+
+function recuperar_senha() {
+    // 1º Carregar os valores dos campos NOME e PALAVRA-PASSE
+    const nome_digitado = document.getElementById("rNome").value;
+    const palavrapasse_digitada = document.getElementById("rPalavraPasse").value;
+
+    const nome_salvo = localStorage.getItem("nome");
+    const palavrapasse_salva = localStorage.getItem("palavrapasse");
+
+// 2º Buscar no localStorage os valores de NOME e PALAVRA-PASSE
+
+
+// 3º Comparar se os valores carregados nos campos da tela
+// são compatíveis com os valores armazenados no localStorage.
+//
+// Se forem iguais, exibir a senha na tela ou em um alert.
+//
+// Se forem diferentes, notificar o usuário na tela ou em um alert
+// informando que os dados não são compatíveis.
+// Além disso, limpar os campos de entrada (inputs).
+
+
+// DESAFIO SEM I.AAAAA:
+//
+// Vocês devem implementar um controle de tentativas para a recuperação de senha.
+//
+// REGRAS:
+// Vocês devem validar a quantidade de tentativas incorretas
+// de NOME e PALAVRA-PASSE.
+//
+// Quando o usuário errar 3 vezes, vocês devem bloquear os campos (inputs)
+// e impedir que o usuário realize uma nova tentativa de recuperação de senha.
+//
+// Lembrem-se de salvar a quantidade de erros no localStorage.
 }
